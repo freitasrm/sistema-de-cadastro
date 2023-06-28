215aed2ac9b0b8344f04075ea6cd4b8d293a4689
Rodrigo Martins <48421138+freitasrm@users.noreply.github.com>
2023-06-28 00:50:38 -0300

    console log indicando modificações do martins

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+console.log("O Martins fez esse commit sem você ver na aula!")
