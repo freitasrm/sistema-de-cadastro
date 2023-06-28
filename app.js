aada3b8f5b2939c68489607b988efcf8c0024448
RMartins <freitas.rodrigom@gmail.com>
2023-06-28 01:05:02 -0300

    voltando para o status do martins

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("O Martins fez esse commit sem você ver na aula!")
-console.log("Enviando para o GitHub")
