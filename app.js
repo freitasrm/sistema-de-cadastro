b4c72399ce1f936014113fa69d3759bf0c2524b7
RMartins <freitas.rodrigom@gmail.com>
2023-06-28 00:56:03 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("O Martins fez esse commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
